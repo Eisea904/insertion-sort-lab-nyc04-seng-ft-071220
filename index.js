b72c26669b5455d4656886358f68855d6968d06d
Alex E(She/Her) <eisea904@newschool.edu>
2021-01-30 15:28:45 -0500

    solved

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
-
+    let minValue = 0
+    let minIndex = 0
+    for (i = 0; i < array.length; i++) {
+        if (array[i] < minValue) {
+            minValue = array[i]
+            minIndex = i
+        }
+    }
+    array.splice(minIndex)
+    return currentMin
 }
 
 function selectionSort(array){
-
+    let arrayCopy = array   // nondestructive, preserve original array
+    let sortedArray = []
+    let currentMin
+    while (array.length !=0) {
+        currentMin = findMinAndRemove(arrayCopy)
+        sortedArray.push(currentMin)
+    }
+    return sortedArray
 }
+
